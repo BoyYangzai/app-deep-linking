2e3540b159618dedff053b0645cbc01338204c68
洋 <94534613+BoyYangzai@users.noreply.github.com>
2023-12-26 11:03:24 +0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+
+  const [deepLink,setDeepLink] = useState('')
   useEffect(() => {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const appNameParam = urlParams.get('appName');
     const appPathParam = urlParams.get('appPath');
 
+    const link = `${appNameParam}://${appPathParam}`
+    setDeepLink(link)
     window.open(
-      `${appNameParam}://${appPathParam}`
+      link,'__blank'
     )
   }, []);
 
 
   return (
     <>
-    
+      {deepLink}
     </>
   )
 }
