a85fb71b7b893e5c41b647cffc1f0777a1aa5fab
洋 <94534613+BoyYangzai@users.noreply.github.com>
2023-12-26 10:30:07 +0800

    feat: app deep linking init

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  useEffect(() => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const appNameParam = urlParams.get('appName');
+    const appPathParam = urlParams.get('appPath');
+
+    window.open(
+      `${appNameParam}://${appPathParam}`
+    )
+  }, []);
+
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
